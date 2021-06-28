c2829b53894fe1d3ec2d18c6bbe72dae91a6d9cc
Nishtha Agarwal <77295838+only4nishtha@users.noreply.github.com>
2021-06-28 17:15:28 +0530

    Update Info1.jsx

--- Info1.jsx
+++ Info1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Info1(){
   return(
-    <div class="note">
+    <div className="note">
       <h1>Web Dev with Javascript and React.Js</h1>
       <p>A basic front end Web Development Bootcamp.</p>
     </div>
