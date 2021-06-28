33da7a26b7129eaf6ae5ddf6f734a3fc8523f989
Nishtha Agarwal <77295838+only4nishtha@users.noreply.github.com>
2021-06-28 17:18:00 +0530

    Update Info3.jsx

--- Info3.jsx
+++ Info3.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Info3(){
   return(
-    <div class="note">
+    <div className="note">
       <h1>Python and Machine Learning</h1>
       <p>A basic bootcamp to learn Python & ML.</p>
     </div>
